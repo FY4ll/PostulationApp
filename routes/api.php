690ef126f2e02b3b32691599854e1b3c69eb5171
FY4ll <fadyallemann@gmail.com>
2023-06-15 16:07:05 +0200

    added 3 new api url for gettings data for postulation per user table

--- routes/api.php
+++ routes/api.php
@@ -26,5 +26,24 @@ Route::get('/user/postulations/count', function (Request $request) {
     $userId = $request->query('user_id');
     $postCounter = DB::table('postulation_user')->where('user_id' ,'=', $userId)->count();
     return response()->json(['numPostulations' => $postCounter]);
+
+});
+Route::get('/user/postulations', function (Request $request){
+    $userId = $request->query('user_id');
+    $postulations = DB::table('postulations')
+        ->select('postulations.*', 'postulation_user.user_id')
+        ->join('postulation_user', 'postulations.id', '=', 'postulation_user.postulation_id')
+        ->where('postulation_user.user_id', '=', $userId)
+        ->get();
+    return response()->json($postulations);
+});
+Route::get('/user/postulation/select', function (Request $request) {
+    $userId = $request->query('user_id');
+    $postCounter = DB::table('postulation_user')
+        ->select('postulation_user.created_at', 'postulations.apprentissage', 'postulations.avancement_postulation', 'postulation_user.updated_at')
+        ->join('postulations', 'postulations.id', '=', 'postulation_user.postulation_id')
+        ->where('postulation_user.user_id', $userId)
+        ->get();
+    return response()->json($postCounter);
 });
 
