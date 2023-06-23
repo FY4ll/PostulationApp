9be51e1e63f6e5319f1528a440ac783c612c6d01
FY4ll <fadyallemann@gmail.com>
2023-06-23 11:56:41 +0200

    Renamed some variable and deleted unusefull function

--- resources/js/Pages/Postulation/colab_postulation/postulation_recap.jsx
+++ resources/js/Pages/Postulation/colab_postulation/postulation_recap.jsx
@@ -10,14 +10,10 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import axios from 'axios';
 import {Button, Dialog, DialogActions, DialogContent, DialogTitle} from '@mui/material';
-import {Control, Field, Form, Label, Message} from '@radix-ui/react-form';
 
 export default function MesPostulation({auth}) {
     const [postulations, setPostulations] = React.useState([]);
-    const [selectedPostulation, setSelectedPostulation] = React.useState(null);
-    const [openDialog, setOpenDialog] = React.useState(false);
-    const [openEditDialog, setOpenEditDialog] = React.useState(false);
-    const [editPostulation, setEditPostulation] = React.useState([]);
+    const [dialogOpen, setDialogOpen] = React.useState(false);
 
     React.useEffect(() => {
         fetchData();
@@ -31,76 +27,9 @@ export default function MesPostulation({auth}) {
             console.error(error);
         }
     };
-    const deletePostulation = (postulationId) => {
-        setPostulations((prevPostulations) =>
-            prevPostulations.filter((postulation) => postulation.id !== postulationId)
-        );
-    };
-    const handleEdit = async (postulation) => {
-        console.log('Editing postulation:', postulation);
-        try {
-            const response = await axios.get('api/postulation_user/select', {
-                params: {
-                    user_id: auth.user.id,
-                    postulation_id: postulation
-                }
-            });
-            setEditPostulation(response.data);
-            setOpenEditDialog(true);
-        } catch (error) {
-            console.error(error);
-        }
-    };
 
-    const handleDelete = async (postulation) => {
-        try {
-            console.log(postulation);
-            await axios.post('api/postulation_user/delete', null, {
-                params: {
-                    postulation_id: postulation
-                }
-            });
-            deletePostulation(postulation)
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    const handleCloseEditDialog = () => {
-        setSelectedPostulation(null);
-        setOpenEditDialog(false);
-        console.log('SAVED');
-    };
-
-    const handleSaveEditDialog = async (event, postulationId) => {
-        setSelectedPostulation(null);
-        setOpenEditDialog(false);
-        const formData = new FormData(event.currentTarget);
-        const update = [formData.get('nom'), formData.get('prenom'), formData.get('mail')]
-        console.log(update)
-        try {
-            const response = await axios.post('api/postulation_user/save', null, {
-                params: {
-                    postulation_id: postulationId,
-                    name: formData.get('nom'),
-                    prenom: formData.get('prenom'),
-                    mail: formData.get('mail')
-                }
-            });
-            console.log(response.data);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    const handleOpenDialog = (postulation) => {
-        setSelectedPostulation(postulation);
-        setOpenDialog(true);
-    };
-
-    const handleCloseDialog = () => {
-        setSelectedPostulation(null);
-        setOpenDialog(false);
+    const handleDialog = () => {
+        setDialogOpen(!dialogOpen);
     };
 
     return (
@@ -130,7 +59,7 @@ export default function MesPostulation({auth}) {
                                 <TableCell align="right">{postulation.situation}</TableCell>
                                 <TableCell align="right">{postulation.created_at}</TableCell>
                                 <TableCell align="right">
-                                    <Button variant="outlined" onClick={() => handleEdit(postulation.id)}>
+                                    <Button variant="outlined" onClick={handleDialog}>
                                         ...
                                     </Button>
                                 </TableCell>
@@ -139,63 +68,14 @@ export default function MesPostulation({auth}) {
                     </TableBody>
                 </Table>
             </TableContainer>
-            <Dialog open={openEditDialog} onClose={handleCloseEditDialog} maxWidth="xl">
-                <DialogTitle>Edit Postulation</DialogTitle>
+            <Dialog open={dialogOpen} onClose={handleDialog} maxWidth="xl">
+                <DialogTitle>Action</DialogTitle>
                 <DialogContent>
-                    {editPostulation.map((postulation) => (
-                        <Form
-                            key={postulation.id}
-                            className="FormRoot"
-                            onSubmit={(event) => handleSaveEditDialog(event, postulation.id)}
-                        >
-                            <Field className="FormField">
-                                <div style={{display: 'flex', alignItems: 'baseline', justifyContent: 'space-between'}}>
-                                    <Label className="FormLabel" htmlFor="nom">
-                                        Nom
-                                    </Label>
-                                    <Message className="FormMessage" match="valueMissing">
-                                        Entrez votre nom
-                                    </Message>
-                                </div>
-                                <Control asChild>
-                                    <input className="Input" type="text" defaultValue={postulation.nom} name="nom"
-                                           id="nom"/>
-                                </Control>
-                            </Field>
-                            <Field className="FormField">
-                                <div style={{display: 'flex', alignItems: 'baseline', justifyContent: 'space-between'}}>
-                                    <Label className="FormLabel" htmlFor="prenom">
-                                        Prénom
-                                    </Label>
-                                    <Message className="FormMessage" match="valueMissing">
-                                        Entrez votre prénom
-                                    </Message>
-                                </div>
-                                <Control asChild>
-                                    <input className="Input" type="text" defaultValue={postulation.prenom} name="prenom"
-                                           id="prenom"/>
-                                </Control>
-                            </Field>
-                            <Field className="FormField">
-                                <div style={{display: 'flex', alignItems: 'baseline', justifyContent: 'space-between'}}>
-                                    <Label className="FormLabel" htmlFor="mail">
-                                        Mail
-                                    </Label>
-                                    <Message className="FormMessage" match="valueMissing">
-                                        Entrez une adresse mail valide
-                                    </Message>
-                                </div>
-                                <Control asChild>
-                                    <input className="Input" type="email" defaultValue={postulation.mail} name="mail"
-                                           id="mail"/>
-                                </Control>
-                            </Field>
-                            <button type="submit">Save</button>
-                        </Form>
-                    ))}
+                    <Button variant="outlined">Télécharger les fichiiers</Button>
+                    <Button variant="outlined">donner son préavis</Button>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleCloseEditDialog}>Cancel</Button>
+                    <Button onClick={handleDialog}>Cancel</Button>
                 </DialogActions>
             </Dialog>
         </AuthenticatedLayout>
