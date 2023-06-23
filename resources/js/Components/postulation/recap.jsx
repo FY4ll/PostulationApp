7523bf349ff6c790a5b707c0b2aa4738b5e2c697
FY4ll <fadyallemann@gmail.com>
2023-06-23 08:36:58 +0200

    added conditional state to show different text for roles

--- resources/js/Components/postulation/recap.jsx
+++ resources/js/Components/postulation/recap.jsx
@@ -1,12 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import {LinearProgress} from "@mui/material";
+import {LinearProgress} from '@mui/material';
 
 export default function Recap({auth}) {
     const [numPostulations, setNumPostulations] = useState(null);
+    const [userRole, setUserRole] = useState(null);
 
     useEffect(() => {
-        // permet de récuperer les postulation des utilisateur via l'id de l'utilisateur
+        getUserRole();
+
+        // Permet de récupérer les postulations des utilisateurs via l'ID de l'utilisateur
         async function fetchNumPostulations() {
             try {
                 const response = await axios.get('/api/user/postulations/count', {
@@ -21,8 +24,24 @@ export default function Recap({auth}) {
             }
         }
 
-        fetchNumPostulations();
+        if (userRole !== 4 || userRole !== 2) {
+            fetchNumPostulations();
+        }
     }, []);
+
+    const getUserRole = async () => {
+        try {
+            const response = await axios.get('api/user_role/select', {
+                params: {
+                    user_id: auth.user.id,
+                }
+            });
+            console.log(response.data[0].role_id);
+            setUserRole(response.data[0].role_id);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     if (numPostulations === null) {
         return <div>Loading...</div>;
@@ -30,11 +49,15 @@ export default function Recap({auth}) {
 
     return (
         <div>
-            Numéro de postulation : {numPostulations}
-            État de votre postulation:
-            <LinearProgress variant="determinate" value={10}/>
+            {userRole === 2 || userRole === 4 ? (
+                <h1>Bonjour {auth.user.name}!</h1>
+            ) : (
+                <div>
+                    Numéro de postulation : {numPostulations}
+                    État de votre postulation:
+                    <LinearProgress variant="determinate" value={10}/>
+                </div>
+            )}
         </div>
-
-
     );
 }
