fd2a299727bf767bea6d5086484c23ffa68a50a9
FY4ll <fadyallemann@gmail.com>
2023-06-23 10:56:00 +0200

    added new migration/table

---
+++ database/migrations/2023_06_23_081254_create_postulation_preavis.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('postulation_preavis', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('postulation_id');
+            $table->unsignedBigInteger('colab_id');
+            $table->timestamps();
+
+            $table->foreign('postulation_id')->references('id')->on('postulations')->onDelete('cascade');
+            $table->foreign('colab_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('postulation_preavis', function (Blueprint $table) {
+            $table->dropForeign(['postulation_id']);
+            $table->dropForeign(['colab_id']);
+        });
+
+        Schema::dropIfExists('postulation_preavis');
+    }
+};
