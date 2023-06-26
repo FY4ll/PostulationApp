744624268d7319b585f31d1e376fb1947c422f4f
FY4ll <fadyallemann@gmail.com>
2023-06-26 11:59:50 +0200

    able to store forms data into variable

--- resources/js/Pages/Postulation/colab_postulation/preavis/preavis_forms.jsx
+++ resources/js/Pages/Postulation/colab_postulation/preavis/preavis_forms.jsx
@@ -1,12 +1,31 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import {Head} from '@inertiajs/react';
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, FormControl, InputLabel, MenuItem, Select, TextField} from '@mui/material';
 
 export default function MesPostulation({auth}) {
+    const [formData, setFormData] = useState({
+        validPostulation: '',
+        explication: '',
+    });
+
     const handleSubmit = (event) => {
         event.preventDefault();
+        // Récupérer les valeurs du formulaire
+        const {validPostulation, explication} = formData;
+
         // Ajoutez ici votre logique de soumission du formulaire
+
+        console.log('Valeur du champ "Valider la postulation" :', validPostulation);
+        console.log('Valeur du champ "Explication de votre choix" :', explication);
+    };
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value,
+        }));
     };
 
     return (
@@ -16,38 +35,47 @@ export default function MesPostulation({auth}) {
         >
             <Head title="Mes Préavis"/>
             <h1>hello world</h1>
-            <form className="FormRoot" action="/" method="post" onSubmit={handleSubmit}>
-                <FormControl required fullWidth>
-                    <InputLabel id="valid-postulation-label">Valider la postulation</InputLabel>
-                    <Select
-                        labelId="valid-postulation-label"
-                        id="valid-postulation"
-                        name="validPostulation"
-                        className="Input"
-                        required
-                    >
-                        <MenuItem value="" disabled>
-                            Veuillez choisir
-                        </MenuItem>
-                        <MenuItem value="Apte">Apte</MenuItem>
-                        <MenuItem value="Inapte">Inapte</MenuItem>
-                    </Select>
-                </FormControl>
+            <div style={{marginTop: '2rem'}}>
+                <form className="FormRoot" action="/" method="post" onSubmit={handleSubmit}>
+                    <FormControl required fullWidth>
+                        <InputLabel id="valid-postulation-label">Valider la postulation</InputLabel>
+                        <Select
+                            labelId="valid-postulation-label"
+                            id="valid-postulation"
+                            name="validPostulation"
+                            className="Input"
+                            style={{zIndex: 1}} // Ajout de la propriété zIndex
+                            required
+                            value={formData.validPostulation}
+                            onChange={handleChange}
+                        >
+                            <MenuItem value="" disabled>
+                                Veuillez choisir
+                            </MenuItem>
+                            <MenuItem value="Apte">Apte</MenuItem>
+                            <MenuItem value="Inapte">Inapte</MenuItem>
+                        </Select>
+                    </FormControl>
 
-                <TextField
-                    className="FormField"
-                    name="Nom"
-                    label="Explication de votre choix"
-                    multiline
-                    rows={6}
-                    required
-                    fullWidth
-                />
+                    <FormControl fullWidth style={{marginTop: '1rem'}}>
+                        <TextField
+                            className="FormField"
+                            name="explication"
+                            label="Explication de votre choix"
+                            multiline
+                            rows={6}
+                            required
+                            fullWidth
+                            value={formData.explication}
+                            onChange={handleChange}
+                        />
+                    </FormControl>
 
-                <Button type="submit" variant="contained" color="primary" style={{marginTop: 10}}>
-                    Envoyer la postulation
-                </Button>
-            </form>
+                    <Button type="submit" variant="contained" color="primary" style={{marginTop: '1rem'}}>
+                        Envoyer la postulation
+                    </Button>
+                </form>
+            </div>
         </AuthenticatedLayout>
     );
 }
