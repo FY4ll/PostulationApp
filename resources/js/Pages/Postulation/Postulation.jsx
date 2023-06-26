3328b3ecdc4689e50cf96a2ec56eab4039a16fc4
FY4ll <fadyallemann@gmail.com>
2023-06-26 11:48:51 +0200

    switched to Material UI for faster and easier design

--- resources/js/Pages/Postulation/Postulation.jsx
+++ resources/js/Pages/Postulation/Postulation.jsx
@@ -1,23 +1,38 @@
 import {useEffect, useState} from 'react';
-import * as Form from '@radix-ui/react-form';
-import {Label} from '@radix-ui/react-form';
+import {
+    Button,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    MenuItem,
+    Radio,
+    RadioGroup,
+    Select,
+    TextField
+} from '@mui/material';
+import {Controller, useForm} from 'react-hook-form';
 import axios from 'axios';
 
 import './style.css';
 
 export default function Postulation({user}) {
+    const {handleSubmit, control} = useForm();
     const [userData, setUserData] = useState(null);
+
     useEffect(() => {
         if (user) {
-            // Mettre à jour les données de l'utilisateur lorsqu'il est disponible
             setUserData(user);
         }
     }, [user]);
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
+    const onSubmit = async (data) => {
+        const formData = new FormData();
         formData.append('id', user.id);
+
+        for (const key in data) {
+            formData.append(key, data[key]);
+        }
+
         try {
             await axios.post('api/postulation', formData);
             console.log('Postulation soumise avec succès');
@@ -28,124 +43,129 @@ export default function Postulation({user}) {
     };
 
     if (!userData) {
-        // Afficher un message de chargement ou effectuer une autre action si les données de l'utilisateur ne sont pas encore disponibles
         return <div>Loading...</div>;
     }
 
-    // Utiliser les informations de l'utilisateur
-
-
     return (
-        <Form.Root className="FormRoot" onSubmit={handleSubmit} action="/postulation" method="post">
-            <Form.Field className="FormField" name="Nom">
-                <div style={{display: 'flex', alignItems: 'baseline', justifyContent: 'space-between'}}>
-                    <Form.Label className="FormLabel">Nom</Form.Label>
-                    <Form.Message className="FormMessage" match="valueMissing">
-                        Entrez votre nom
-                    </Form.Message>
-                </div>
-                <Form.Control asChild>
-                    <input className="Input" type="text" required/>
-                </Form.Control>
-            </Form.Field>
-            <Form.Field className="FormField" name="Prenom">
-                <div style={{display: 'flex', alignItems: 'baseline', justifyContent: 'space-between'}}>
-                    <Form.Label className="FormLabel">Prénom</Form.Label>
-                    <Form.Message className="FormMessage" match="valueMissing">
-                        Entrez votre Prénom
-                    </Form.Message>
-                </div>
-                <Form.Control asChild>
-                    <input className="Input" type="text" required/>
-                </Form.Control>
-            </Form.Field>
-            <Form.Field className="FormField" name="Mail">
-                <div style={{display: 'flex', alignItems: 'baseline', justifyContent: 'space-between'}}>
-                    <Form.Label className="FormLabel">Mail</Form.Label>
-                    <Form.Message className="FormMessage" match="valueMissing">
-                        Entrez Une adresse mail valide
-                    </Form.Message>
-                </div>
-                <Form.Control asChild>
-                    <input className="Input" type="email" required/>
-                </Form.Control>
-            </Form.Field>
+        <form className="FormRoot" onSubmit={handleSubmit(onSubmit)} action="/postulation" method="post">
+            <Controller
+                name="Nom"
+                control={control}
+                defaultValue=""
+                rules={{required: true}}
+                render={({field}) => (
+                    <FormControl className="FormField" fullWidth>
+                        <FormLabel>Nom</FormLabel>
+                        <TextField {...field} required/>
+                    </FormControl>
+                )}
+            />
 
+            <Controller
+                name="Prenom"
+                control={control}
+                defaultValue=""
+                rules={{required: true}}
+                render={({field}) => (
+                    <FormControl className="FormField" fullWidth>
+                        <FormLabel>Prénom</FormLabel>
+                        <TextField {...field} required/>
+                    </FormControl>
+                )}
+            />
 
-            <Form.Field name="Dropdown">
-                <Label className="FormLabel">Apprentissage</Label>
-                <Form.Message className="FormMessage" match="valueMissing">
-                    Veuillez sélectionner un apprentissage
-                </Form.Message>
-                <Form.Control asChild>
-                    <select name="Apprentissage" id="job" className="Input" required>
-                        <option value="" disabled selected hidden>Veuillez choisir</option>
-                        <option value="Informaticien">Informaticien (développement)</option>
-                        <option value="Employer de commerce">Employer de commerce</option>
-                    </select>
-                </Form.Control>
-            </Form.Field>
-            <Form.Field name="Radio">
-                <Label className="FormLabel">Emploi Actuel</Label>
-                <Form.Message className="FormMessage" match="valueMissing">
-                    Veuillez sélectionner un statut
-                </Form.Message>
-                <Form.Control asChild>
-                    <div className="radio-group">
-                        <label htmlFor="option1" className="radio-label">Étudiant</label>
-                        <input type="radio" id="option1" name="situation" value="etudiant" className="radio-input"/>
-                        <label htmlFor="option2" className="radio-label">Employé</label>
-                        <input type="radio" id="option2" name="situation" value="Employe" className="radio-input"/>
-                        <label htmlFor="option3" className="radio-label">Indépendant</label>
-                        <input type="radio" id="option3" name="situation" value="Independant" className="radio-input"/>
-                        <label htmlFor="option4" className="radio-label">Sans emploi</label>
-                        <input type="radio" id="option4" name="situation" value="sans-emploi" className="radio-input"/>
-                    </div>
-                </Form.Control>
-            </Form.Field>
-            {/* Barre de téléchargement de fichier 1 */}
-            <Form.Field className="FormField" name="file1">
-                <div>
-                    <Form.Label className="FormLabel">CV</Form.Label>
-                    <Form.Message className="FormMessage" match="valueMissing">
-                        Format PDF uniquement
-                    </Form.Message>
-                </div>
-                <Form.Control asChild>
-                    <input className="Input" type="file" accept=".pdf" required/>
-                </Form.Control>
-            </Form.Field>
+            <Controller
+                name="Mail"
+                control={control}
+                defaultValue=""
+                rules={{required: true, pattern: /^\S+@\S+$/i}}
+                render={({field}) => (
+                    <FormControl className="FormField" fullWidth>
+                        <FormLabel>Mail</FormLabel>
+                        <TextField {...field} type="email" required/>
+                    </FormControl>
+                )}
+            />
 
-            {/* Barre de téléchargement de fichier 2 */}
-            <Form.Field className="FormField" name="file2">
-                <div>
-                    <Form.Label className="FormLabel">Lettre de motivation</Form.Label>
-                    <Form.Message className="FormMessage" match="valueMissing">
-                        Format PDF uniquement
-                    </Form.Message>
-                </div>
-                <Form.Control asChild>
-                    <input className="Input" type="file" accept=".pdf" required/>
-                </Form.Control>
-            </Form.Field>
+            <Controller
+                name="Dropdown"
+                control={control}
+                defaultValue=""
+                rules={{required: true}}
+                render={({field}) => (
+                    <FormControl className="FormField" fullWidth>
+                        <FormLabel>Apprentissage</FormLabel>
+                        <Select {...field} required>
+                            <MenuItem value="" disabled>
+                                Veuillez choisir
+                            </MenuItem>
+                            <MenuItem value="Informaticien">Informaticien (développement)</MenuItem>
+                            <MenuItem value="Employer de commerce">Employer de commerce</MenuItem>
+                        </Select>
+                    </FormControl>
+                )}
+            />
 
-            {/* Barre de téléchargement de fichier 3 */}
-            <Form.Field className="FormField" name="file3">
-                <div>
-                    <Form.Label className="FormLabel">Video de motivation</Form.Label>
-                    <Form.Message className="FormMessage" match="valueMissing">
-                        Format video uniquement
-                    </Form.Message>
-                </div>
-                <Form.Control asChild>
-                    <input className="Input" type="file" accept="video/*" required/>
-                </Form.Control>
-            </Form.Field>
-            <Form.Submit asChild>
-                <button className="Button" style={{marginTop: 10}}>
-                    Envoyer la postulation
-                </button>
-            </Form.Submit>
-        </Form.Root>
+            <Controller
+                name="Radio"
+                control={control}
+                defaultValue=""
+                rules={{required: true}}
+                render={({field}) => (
+                    <FormControl className="FormField" fullWidth>
+                        <FormLabel>Emploi Actuel</FormLabel>
+                        <RadioGroup {...field}>
+                            <FormControlLabel value="etudiant" control={<Radio/>} label="Étudiant"/>
+                            <FormControlLabel value="Employe" control={<Radio/>} label="Employé"/>
+                            <FormControlLabel value="Independant" control={<Radio/>} label="Indépendant"/>
+                            <FormControlLabel value="sans-emploi" control={<Radio/>} label="Sans emploi"/>
+                        </RadioGroup>
+                    </FormControl>
+                )}
+            />
+
+            <Controller
+                name="file1"
+                control={control}
+                defaultValue=""
+                rules={{required: true}}
+                render={({field}) => (
+                    <FormControl className="FormField">
+                        <FormLabel>CV</FormLabel>
+                        <input {...field} type="file" accept=".pdf" required/>
+                    </FormControl>
+                )}
+            />
+
+            <Controller
+                name="file2"
+                control={control}
+                defaultValue=""
+                rules={{required: true}}
+                render={({field}) => (
+                    <FormControl className="FormField">
+                        <FormLabel>Lettre de motivation</FormLabel>
+                        <input {...field} type="file" accept=".pdf" required/>
+                    </FormControl>
+                )}
+            />
+
+            <Controller
+                name="file3"
+                control={control}
+                defaultValue=""
+                rules={{required: true}}
+                render={({field}) => (
+                    <FormControl className="FormField">
+                        <FormLabel>Video de motivation</FormLabel>
+                        <input {...field} type="file" accept="video/*" required/>
+                    </FormControl>
+                )}
+            />
+
+            <Button type="submit" variant="contained" color="primary" style={{marginTop: 10}}>
+                Envoyer la postulation
+            </Button>
+        </form>
     );
 }
--- resources/js/Pages/Postulation/colab_postulation/preavis/preavis_forms.jsx
+++ resources/js/Pages/Postulation/colab_postulation/preavis/preavis_forms.jsx
@@ -1,9 +1,13 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import {Head} from '@inertiajs/react';
 import React from 'react';
+import {Button, FormControl, InputLabel, MenuItem, Select, TextField} from '@mui/material';
 
 export default function MesPostulation({auth}) {
-
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Ajoutez ici votre logique de soumission du formulaire
+    };
 
     return (
         <AuthenticatedLayout
@@ -11,7 +15,39 @@ export default function MesPostulation({auth}) {
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Mes Préavis</h2>}
         >
             <Head title="Mes Préavis"/>
-            <h1> hello world</h1>
+            <h1>hello world</h1>
+            <form className="FormRoot" action="/" method="post" onSubmit={handleSubmit}>
+                <FormControl required fullWidth>
+                    <InputLabel id="valid-postulation-label">Valider la postulation</InputLabel>
+                    <Select
+                        labelId="valid-postulation-label"
+                        id="valid-postulation"
+                        name="validPostulation"
+                        className="Input"
+                        required
+                    >
+                        <MenuItem value="" disabled>
+                            Veuillez choisir
+                        </MenuItem>
+                        <MenuItem value="Apte">Apte</MenuItem>
+                        <MenuItem value="Inapte">Inapte</MenuItem>
+                    </Select>
+                </FormControl>
+
+                <TextField
+                    className="FormField"
+                    name="Nom"
+                    label="Explication de votre choix"
+                    multiline
+                    rows={6}
+                    required
+                    fullWidth
+                />
+
+                <Button type="submit" variant="contained" color="primary" style={{marginTop: 10}}>
+                    Envoyer la postulation
+                </Button>
+            </form>
         </AuthenticatedLayout>
     );
 }
